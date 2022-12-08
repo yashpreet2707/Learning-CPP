5feacde04b40382b6390c5a9b381c8cd9c875686
yashpreet2707 <yashpreet1686@gmail.com>
2022-12-08 10:13:46 +0530

    added rest files

---
+++ Code Help Lectures/2_Type-casting--neg-nos.cpp
@@ -0,0 +1,27 @@
+ // Type-casting
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a='a';
+    cout << a << endl;
+
+    char ch=98;
+    cout << ch << endl;
+
+    // char ch1 = 123456;
+    // cout << ch1 << endl;     it is way to big for char storage (1byte=8bit)- prints @(coz that's in the range - ACSII table)
+
+
+    // default and that - unsigned int 
+
+    unsigned int y=112;
+    cout << y << endl;
+
+    unsigned int z=-112;
+    cout << z << endl;
+
+
+
+}
